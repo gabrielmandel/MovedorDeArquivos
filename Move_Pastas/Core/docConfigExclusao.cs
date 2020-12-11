4351851096affa20def84654ccd3a0aa0eea3c6b
Guilherme Soares <guilhermes@partnersistemas.com.br>
2020-12-11 12:50:12 -0300

    Adicionado o Core para auxilio das tarefas

---
+++ Move_Pastas/Core/docConfigExclusao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Move_Pastas.Core
+{
+    public class docConfigExclusao
+    {
+        public string tipo { get; set; }
+        public string NomeExclusao { get; set; }
+    }
+}
---
+++ Move_Pastas/Core/docConfigExclusaoService.cs
@@ -0,0 +1,47 @@
+﻿using Move_Pastas.Core.functions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Move_Pastas.Core
+{
+    public class docConfigExclusaoService
+    {
+        private string pathconfig = "configExclusao.json";
+        public void adicionarItem(docConfigExclusao exclusao)
+        {
+            List<docConfigExclusao> exclusoes =  this.BuscarItensCadastrados();
+
+            if(exclusoes == null)
+            {
+                exclusoes = new List<docConfigExclusao>();
+            }
+
+            exclusoes.Add(exclusao);
+
+            CadastrarConfigExclusao(exclusoes);
+        }
+
+
+        public List<docConfigExclusao> BuscarItensCadastrados()
+        {
+            var arquivo = new Functions().BuscarArquivo(pathconfig);
+
+            if (arquivo != null)
+            {
+                return  JsonConvert.DeserializeObject<List<docConfigExclusao>>(arquivo);
+            }
+
+            return null;
+        }
+
+        public void CadastrarConfigExclusao(List<docConfigExclusao> exclusoes)
+        {
+            string json = JsonConvert.SerializeObject(exclusoes);
+            new Functions().EcreverArquivo(pathconfig, json);
+        }
+    }
+}
